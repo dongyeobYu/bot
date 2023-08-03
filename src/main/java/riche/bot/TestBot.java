408acd4851f7d6f46cfb93c878f86f187c08e925
dongyeobYu <maintain98@naver.com>
2023-08-03 17:27:30 +0900

    bot start

--- src/main/java/riche/bot/TestBot.java
+++ src/main/java/riche/bot/TestBot.java
@@ -3,6 +3,7 @@ package riche.bot;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestBot implements ApplicationListener<ApplicationStartedEvent> {
 
+    botToken botToken;
+
+    @Autowired
+    public void setBotToken(botToken botToken) {
+        this.botToken = botToken;
+    }
 
     public void testBot() {
-        String token = "";
+
+        String token = botToken.getToken();
 
         DiscordApi api = new DiscordApiBuilder().addIntents(Intent.MESSAGE_CONTENT).setToken(token).login().join();
 
