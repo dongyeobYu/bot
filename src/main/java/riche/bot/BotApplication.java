0527d53b4b1006273ef2b78e9c34640079b7e9a3
yudong-yeob <maintain98@naver.com>
2023-08-05 20:42:22 +0900

    gitignore

--- src/main/java/riche/bot/BotApplication.java
+++ src/main/java/riche/bot/BotApplication.java
@@ -1,15 +1,12 @@
 package riche.bot;
 
-import org.javacord.api.DiscordApi;
-import org.javacord.api.DiscordApiBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class BotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 
 		SpringApplication.run(BotApplication.class, args);
 	}
